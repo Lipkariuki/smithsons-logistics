a079f730c304b945ffc7b62ebade476e8fc19e78
Philip Kariuki <lipkariuki@gmail.com>
2025-07-10 18:23:57 +0300

    driverexpense add expense route

--- routers/driver_expense.py
+++ routers/driver_expense.py
@@ -4,7 +4,7 @@ from typing import List
 from database import get_db
 from models import Expense, User
 from schemas import ExpenseCreate, ExpenseOut
-from auth import get_current_user
+from routers.auth import get_current_user
 
 router = APIRouter(prefix="/driver/expenses", tags=["Driver Expenses"])
 
